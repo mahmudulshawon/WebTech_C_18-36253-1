3c2140ffc65375faa5b49e1d1c30bbdf9281f9e1
Mahmudul Shawon <91493134+mahmudulshawon@users.noreply.github.com>
2022-06-19 00:15:13 +0600

    Task 3 uploaded

---
+++ Task 3/Task 3.3/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Profile Picture</title>
+</head>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <label>Profile Picture</label><br><br>
+
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+
+</form>
+
+</body>
+</html>
